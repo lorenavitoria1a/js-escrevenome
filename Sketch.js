8ad6c478dfa5e93c40d9f2333842cf7e389819f8
lorenavitoria1a <araujo.oliveira.lorena@escola.pr.gov.br>
2024-09-03 20:28:36 -0300

    Create Sketch.js

---
+++ Sketch.js
@@ -0,0 +1,9 @@
+https://editor.p5js.org/lorenavitoria/sketches/KsQYHV08y
+função septu ()
+criarCanvas (400.400);
+função draw ()
+reconhecimento (220)
+função draw ()
+fundo ("branco") 
+
+retângulo (0, 10, 100, 150)
